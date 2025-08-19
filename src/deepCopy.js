116c44af2d9b85074c16952fba2fc5b462356991
Eduard Galikhanov <godlik04@mail.ru>
2025-08-19 03:07:34 +0300

    feat: DeepCopy

--- src/deepCopy.js
+++ src/deepCopy.js
@@ -1,3 +1,4 @@
+// Рабочий черновой вариант
 const deepCopy = (obj) => {
 	const isArray = Array.isArray(obj);
 	const copy = isArray ? [] : {};
@@ -25,4 +26,65 @@ const deepCopy = (obj) => {
 	}
 
 	return copy;
+};
+
+// Отрефакторенный вариант
+
+const deepCopy = (input) => {
+	const isArray = Array.isArray(input);
+	const copyObject = isArray ? [] : {};
+
+	if (typeof input !== "object" || input === null) return input;
+
+	for (const [key, value] of Object.entries(input)) {
+		copyObject[key] = deepCopy(value);
+	}
+
+	return copyObject;
+};
+
+const testObject = {
+	a: [{}],
+	b: {
+		d: 9,
+		g: {
+			f: 2,
+		},
+	},
+	d: {
+		n: [2, 4],
+	},
+};
+
+console.log(deepCopy(testObject));
+
+//Канон ~lodash
+
+const deepClone = (input, hash = new WeakMap()) => {
+	if (input === null || typeof input !== "object") {
+		return input; // примитивы возвращаем как есть
+	}
+
+	if (hash.has(input)) {
+		return hash.get(input); // обработка циклических ссылок
+	}
+
+	if (input instanceof Date) {
+		return new Date(input);
+	}
+
+	if (input instanceof RegExp) {
+		return new RegExp(input);
+	}
+
+	const result = Array.isArray(input) ? [] : {};
+	hash.set(input, result);
+
+	for (const key in input) {
+		if (input.hasOwnProperty(key)) {
+			result[key] = deepClone(input[key], hash);
+		}
+	}
+
+	return result;
 };
